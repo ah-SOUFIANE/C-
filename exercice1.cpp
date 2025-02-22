db3d1831dc346c5b600a04ca7954fec47e0707f6
ah-SOUFIANE <s.aithmad2425@uca.ac.ma>
2025-02-22 10:19:44 +0100

    Mise à jour du code avec les nouvelles modifications

--- exercice1.cpp
+++ exercice1.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 
 int main() {
-    int arr[] = {1, 2, 3, 2, 1};  // Example input
+    int arr[] = {1, 2, 3, 2, 1};  
     int n = sizeof(arr) / sizeof(arr[0]);
     
     int unique = 0;
     
     for (int i = 0; i < n; i++) {
-        unique ^= arr[i];  // XOR all elements
+        unique ^= arr[i];  
     }
     
     std::cout << "Le nombre unique est : " << unique << std::endl;
